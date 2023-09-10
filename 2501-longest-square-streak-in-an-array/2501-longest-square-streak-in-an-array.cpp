1c81d006df49918c898af6e4ba701055be367a3e
Usef saad <88557193+yousef132@users.noreply.github.com>
2023-09-10 19:15:45 +0300

    Time: 241 ms (52.56%), Space: 107.4 MB (48.13%) - LeetHub

---
+++ 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+     int longestSquareStreak(vector<int>& nums) {
+        set<long long> mySet;
+        for(auto num : nums) {
+            mySet.insert(num);
+        }
+        int maxCount=0;
+        for(auto i=mySet.begin();i!=mySet.end();i=mySet.begin()){
+            int count=1;
+            long long ft=*i;
+            if(ft>320) break;
+            while(mySet.count(ft*ft)==1){
+                count++;
+                mySet.erase(ft);
+                ft=ft*ft;
+            }
+            mySet.erase(ft);
+            maxCount=max(count,maxCount);
+        }
+        if(maxCount==1 || maxCount==0) return -1;
+        return maxCount;
+    }
+    
+};
