08d08c40dc9f4c45c2e9aca3507c8cc08dcca49b
Usef saad <88557193+yousef132@users.noreply.github.com>
2023-11-07 11:33:34 +0200

    Time: 708 ms (77.42%), Space: 0B (100.00%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -1,12 +1,5 @@
-SELECT
-    p.product_name,
-    SUM(o.unit) AS unit
-FROM
-    Products p
-        JOIN Orders o
-        ON p.product_id = o.product_id
+SELECT p.product_name,SUM(o.unit) AS unit
+FROM Products p JOIN Orders o ON p.product_id = o.product_id
             AND o.order_date BETWEEN '2020-02-01' AND '2020-02-29'
-GROUP BY
-    p.product_name
-HAVING
-    SUM(o.unit) >= 100
+GROUP BY p.product_name
+HAVING SUM(o.unit) >= 100
