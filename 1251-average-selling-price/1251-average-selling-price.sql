7840c1a9561bcaf2b2a97288c7ec7ad379e598d0
Usef saad <88557193+yousef132@users.noreply.github.com>
2023-11-10 08:06:25 +0200

    Time: 645 ms (64.95%), Space: 0B (100.00%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -2,6 +2,7 @@ select p.product_id , isnull(round(sum(units*price)/sum(cast (units as float))  
 
 from 
 Prices p left   join UnitsSold u
-on u.product_id =p.product_id and u.purchase_date >= p.start_date  and u.purchase_date <= p.end_date
+on u.product_id =p.product_id and u.purchase_date between p.start_date  and  p.end_date
 group by p.product_id
+
 
