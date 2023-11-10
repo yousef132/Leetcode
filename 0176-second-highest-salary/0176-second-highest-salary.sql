9ab6f6dd5a77e37a6fcd86b67526ad631f534a5f
Usef saad <88557193+yousef132@users.noreply.github.com>
2023-11-10 10:32:37 +0200

    Time: 939 ms (34.81%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,7 +1,17 @@
-SELECT MAX(salary) 
-AS SecondHighestSalary
-FROM Employee 
-WHERE salary 
-NOT IN ( 
-    SELECT MAX(salary) 
-    FROM Employee)
+SELECT
+  CASE WHEN EXISTS (
+    SELECT 1
+    FROM (
+      SELECT salary, DENSE_RANK() OVER (ORDER BY salary DESC) AS rn
+      FROM Employee
+    ) AS newtable
+    WHERE rn = 2
+  ) THEN
+    (SELECT TOP 1 SecondHighestSalary FROM (
+      SELECT salary AS SecondHighestSalary, DENSE_RANK() OVER (ORDER BY salary DESC) AS rn
+      FROM Employee
+    ) AS newtable
+    WHERE rn = 2)
+  ELSE
+    NULL
+  END AS SecondHighestSalary;
