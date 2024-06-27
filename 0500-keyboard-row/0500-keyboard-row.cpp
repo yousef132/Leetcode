636ff092ea3679391c42f6bfa1cf828d0b2d93eb
Usef saad <88557193+yousef132@users.noreply.github.com>
2024-06-27 14:15:28 +0300

    Time: 0 ms (100.00%), Space: 8.3 MB (35.55%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+   vector<string> findWords(vector<string>& words) {
+    string s1 = "qwertyuiop", s2 = "asdfghjkl", s3 = "zxcvbnm";
+    map<char, int> mp;
+    for (int i = 0; i < s1.size(); ++i) {
+        mp[s1[i]] = 1;
+    }
+    for (int i = 0; i < s2.size(); ++i) {
+        mp[s2[i]] = 2;
+    }
+    for (int i = 0; i < s3.size(); ++i) {
+        mp[s3[i]] = 3;
+    }
+    vector<string>ans;
+    for (int i = 0; i <words.size() ; ++i) {
+
+        set<int> st;
+        for (int j = 0; j <words[i].size(); ++j) {
+            st.insert(mp[tolower(words[i][j])]);
+        }
+        if(st.size() ==1)
+            ans.push_back(words[i]);
+    }
+    return ans;
+}
+
+};
